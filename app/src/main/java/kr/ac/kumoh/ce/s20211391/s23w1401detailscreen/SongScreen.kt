3f67e6697bb53d5d1a4841bfa044f8178883316d
최연우 <jjangguzi@naver.com>
2023-12-07 20:05:07 +0900

    songId, songGenre updated

--- app/src/main/java/kr/ac/kumoh/ce/s20211391/s23w1401detailscreen/SongScreen.kt
+++ app/src/main/java/kr/ac/kumoh/ce/s20211391/s23w1401detailscreen/SongScreen.kt
@@ -1,9 +1,12 @@
 package kr.ac.kumoh.ce.s20211391.s23w1401detailscreen
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
@@ -28,29 +31,37 @@ fun SongApp(){
             SongList(navController)
         }
         composable(
-            route = SongScreen.Detail.name + "/{songId}",
-            arguments = listOf(navArgument("songId"){
-                type = NavType.StringType
-            })
+            route = SongScreen.Detail.name + "/{songId},{songGenre}",
+            arguments = listOf(
+                navArgument("songId"){ type = NavType.StringType },
+                navArgument("songGenre"){ type = NavType.StringType })
         ){
-            SongDetail(it.arguments?.getString("songId"))
+            SongDetail(
+                it.arguments?.getString("songId"),
+                it.arguments?.getString("songGenre")
+            )
         }
     }
 }
 
 @Composable
 fun SongList(navController: NavController){
-    Column{
-        Button(onClick = {navController.navigate(SongScreen.Detail.name + "/song01")}) {
-            Text("노래 1")
-        }
-        Button(onClick = {navController.navigate(SongScreen.Detail.name + "/song02")}) {
-            Text("노래 2")
+    LazyColumn{
+        items(50) {
+            Button(
+                modifier = Modifier.fillMaxWidth(),
+                onClick = { navController.navigate(SongScreen.Detail.name + "/${it},발라드")},
+                ) {
+                Text("노래 $it")
+            }
         }
     }
 }
 
 @Composable
-fun SongDetail(songId: String?){
-    Text("노래 내용: $songId")
+fun SongDetail(songId: String?, songGenre: String?){
+    Column {
+        Text("노래 $songId")
+        Text("장르 $songGenre")
+    }
 }
